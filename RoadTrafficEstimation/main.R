79375b32ceef57bd566ef38291d77db0adf1298c
GwnGuillaume <gwenael.guillaume@cerema.fr>
2026-02-19 16:12:36 +0100

    Fixed sources of bootstrap and co

--- RoadTrafficEstimation/main.R
+++ RoadTrafficEstimation/main.R
@@ -3,41 +3,69 @@
 # ==============================================================================
 
 # ------------------------------------------------------------------------------
-# R version
+# Force working directory to project root
 # ------------------------------------------------------------------------------
-
-message("💻 ", R.version.string)
-
-get_script_path <- function() {
-  # Rscript / bash mode
-  args <- commandArgs(trailingOnly = FALSE)
-  file_arg <- "--file="
-  script_path <- sub(pattern = file_arg, replacement = "", x = args[grep(file_arg, args)])
-  if (length(script_path) > 0) {
-    return(normalizePath(path = script_path))
+if (!exists("PROJECT_ROOT")) {
+  # Option 1: use the path given in the script .sh as environment variable
+  PROJECT_ROOT <- Sys.getenv("PROJECT_ROOT")
+  if (PROJECT_ROOT == "") {
+    # Option 2: fallback -> current repository
+    PROJECT_ROOT <- getwd()
   }
-  # VS Code / source() mode
-  if (!is.null(sys.frames()[[1]]$ofile)) {
-    return(normalizePath(path = sys.frames()[[1]]$ofile))
-  }
-  # Interactive fallback
-  return(normalizePath(path = file.path(getwd(), "main.R")))
 }
-
-# ------------------------------------------------------------------------------
-# Force working directory = script directory
-# ------------------------------------------------------------------------------
-script_path  <- get_script_path()
-PROJECT_ROOT <- dirname(path = script_path)
-
 setwd(PROJECT_ROOT)
 
-message("💻 Current working directory: ", getwd())
+message(sprintf("📁 Current working directory: %s", PROJECT_ROOT))
+
+# ------------------------------------------------------------------------------
+# Pipeline mode (CLI argument)
+# ------------------------------------------------------------------------------
+
+args <- commandArgs(trailingOnly = TRUE)
+MODE <- if (length(args) >= 1) {
+  args[1]
+} else {
+  "data_prep"
+}
+assign("MODE", MODE, envir = .GlobalEnv)
+
+message(sprintf("🚦 Pipeline mode: %s", 
+                ifelse(test = MODE=="avatar", 
+                       yes = paste(MODE, "download"), 
+                       no = MODE)))
 
 # ------------------------------------------------------------------------------
 # Run successive steps
 # ------------------------------------------------------------------------------
 
-source("bootstrap.R")
-source("config_pipeline.R")
-source("run_pipeline.R")
+# Bootstrap configuration
+source("bootstrap/bootstrap.R")
+source("config/config_global.R")
+source("config/config_data_prep.R")
+source("config/config_training.R")
+source("config/config_forecast.R")
+
+# Complete configuration list
+CFG <- list(
+  global    = CONFIG_GLOBAL, 
+  data_prep = CONFIG_DATA_PREP, 
+  training  = CONFIG_TRAINING, 
+  forecast  = CONFIG_FORECAST)
+assign("CFG", CFG, envir = .GlobalEnv)
+
+# --------------------------------------------------
+# Register pipeline modes (depends on CFG)
+# --------------------------------------------------
+source("./config/mode_registry.R")
+if (!MODE %in% names(MODE_REGISTRY)) {
+  stop("Unknown MODE: ", MODE)
+}
+# Logs
+LOG_FILE <- MODE_REGISTRY[[MODE]]$log_file
+assign("LOG_FILE", LOG_FILE, envir = .GlobalEnv)
+
+# Setup
+source("project_setup.R")
+
+# Run selected pipeline
+source(MODE_REGISTRY[[MODE]]$script)
