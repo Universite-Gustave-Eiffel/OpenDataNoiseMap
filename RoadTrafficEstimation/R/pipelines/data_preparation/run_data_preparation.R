f1904703032450a707c1d6fca6da8d71f974ade2
GwnGuillaume <gwenael.guillaume@cerema.fr>
2026-02-20 12:47:59 +0100

    Corrected paths for data preparation scripts

--- RoadTrafficEstimation/R/pipelines/data_preparation/run_data_preparation.R
+++ RoadTrafficEstimation/R/pipelines/data_preparation/run_data_preparation.R
@@ -5,10 +5,10 @@
 # ------------------------------------------------------------------------------
 # Run pipeline steps
 # ------------------------------------------------------------------------------
-#source("pipelines/data_preparation/01_osm_download.R")
-source("pipelines/data_preparation/02_osm_processing.R")
-source("pipelines/data_preparation/03_osm_feature_engineering.R")
-source("pipelines/data_preparation/04_avatar_download.R")
-source("pipelines/data_preparation/05_avatar_aggregation.R")
-# source("pipelines/data_preparation/05_feature_engineering.R")
-source("pipelines/data_preparation/06_training_dataset_merge.R")
+#source("R/pipelines/data_preparation/01_osm_download.R")
+source("R/pipelines/data_preparation/02_osm_processing.R")
+source("R/pipelines/data_preparation/03_osm_feature_engineering.R")
+source("R/pipelines/data_preparation/04_avatar_download.R")
+source("R/pipelines/data_preparation/05_avatar_aggregation.R")
+# source("R/pipelines/data_preparation/05_feature_engineering.R")
+source("R/pipelines/data_preparation/06_training_dataset_merge.R")
