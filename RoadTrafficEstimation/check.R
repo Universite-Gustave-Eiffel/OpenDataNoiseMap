471a8d1f2fe6437cd010a8665cb933335e40433b
GwnGuillaume <gwenael.guillaume@cerema.fr>
2026-02-23 16:19:18 +0100

    Removed unnecessary folders and files

--- RoadTrafficEstimation/check.R
+++
@@ -1,9 +0,0 @@
-library(sf)
-
-gpkg_path <- "./RoadTrafficEstimation/data/osm/gpkg/02_osm_network_france_engineered.gpkg"
-
-st_layers(gpkg_path)
-
-x <- st_read(gpkg_path, quiet = FALSE)
-dim(x)
-names(x)
